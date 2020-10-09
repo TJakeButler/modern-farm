2069305ec783946189796c7c6a57053b5fce6b4f
Jake Butler <tjakebutler@gmail.com>
2020-10-09 16:14:51 -0500

    Added field.js and began to follow instructions on creating an empty fieldArray and creating addPlant and usePlants functions which are exported to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+import { addPlant, usePlants } from "./field.js";
 // Start of JS Logic
 
 
@@ -7,23 +8,29 @@ import { createCorn } from "./seeds/corn.js";
 import { createPotato } from "./seeds/potato.js";
 import { createSoybean } from "./seeds/soybean.js";
 import { createSunflower } from "./seeds/sunflower.js";
+import { createWheat } from "./seeds/wheat.js";
+import {addPlant, usePlants} from "./field.js";
 
 
+const addPlantMain = addPlant();
+const usePlantMain = usePlants();
 
+const yearlyPlan = createPlan(); 
 
 const asparagusSeed = createAsparagus();
-const yearlyPlan = createPlan(); 
 const potatoSeed = createPotato();
 const soybeanSeed = createSoybean();
 const sunflowerSeed = createSunflower();
-
+const wheatSeed = createWheat();
 const cornSeed = createCorn();
 
 console.log("Welcome to the main module")
 
 console.log(cornSeed);
+console.log(wheatSeed);
 console.log(potatoSeed);
 console.log(asparagusSeed);
 console.log(sunflowerSeed);
 console.log(soybeanSeed);
+
 console.log(yearlyPlan);
