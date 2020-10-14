7db0e34d0e90c28a2ffced0ab4b172033418ad95
Jake Butler <tjakebutler@gmail.com>
2020-10-14 13:18:33 -0500

    Created the tractor.js file and inside of that file created the plantSeeds function which iterates through the nested arrays of elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,18 @@
-// Start of JS Logic
+// import { createPlan } from "./plan.js";
+// import { plantSeeds } from "./tractor.js";
 
 
-import { createPlan } from "./plan.js"
-import { createAsparagus } from "./seeds/asparagus.js"
-import { createCorn } from "./seeds/corn.js";
-import { createPotato } from "./seeds/potato.js";
-import { createSoybean } from "./seeds/soybean.js";
-import { createSunflower } from "./seeds/sunflower.js";
-import { createWheat } from "./seeds/wheat.js";
-import {addPlant, usePlants} from "./field.js";
+import { createPlan } from "./plan.js";
+import { usePlants } from "./field.js"
+import { plantSeeds } from "./tractor.js";
 
 
-const addPlantMain = addPlant(createAsparagus());
-const usePlantMain = usePlants();
-
-const yearlyPlan = createPlan(); 
-
-const asparagusSeed = createAsparagus();
-const potatoSeed = createPotato();
-const soybeanSeed = createSoybean();
-const sunflowerSeed = createSunflower();
-const wheatSeed = createWheat();
-const cornSeed = createCorn();
-
-console.log("Welcome to the main module")
-console.log(addPlantMain)
-console.log(usePlantMain)
 
 
-// console.log(cornSeed);
-// console.log(wheatSeed);
-// console.log(potatoSeed);
-// console.log(asparagusSeed);
-// console.log(sunflowerSeed);
-// console.log(soybeanSeed);
+let yearlyPlan = createPlan();
 
-// console.log(yearlyPlan);
+
+plantSeeds(yearlyPlan);
+
+console.log(usePlants());
+
